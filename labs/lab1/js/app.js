2805883947114c8497e9148c195b6c75f197e145
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-02 11:43:41 -0400

    condensed

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,16 +1,11 @@
 function setup(){
     createCanvas(600,800);
     background(0,50,205);
-}
 
-function draw(){
     fill(120, 0, 120, 20);
     ellipse (60,80,100,100);
     noStroke();
 
-}
-
-function draw(){
     fill(255);
     arc(350, 500, 400, 400, PI, TWO_PI,);
 }
