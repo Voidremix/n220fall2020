49c488f0cab7f8d2461933ae3dc791a39e611424
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-02 11:44:36 -0400

    noStroke master

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,10 +1,10 @@
 function setup(){
     createCanvas(600,800);
     background(0,50,205);
-
-    fill(120, 0, 120, 20);
-    ellipse (60,80,100,100);
     noStroke();
+
+    fill(120, 0, 120);
+    ellipse (60,80,100,100);
 
     fill(255);
     arc(350, 500, 400, 400, PI, TWO_PI,);
