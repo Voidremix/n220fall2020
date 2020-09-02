665bbc09b2bac703a33bc800f2fe0f612e9d232f
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-02 11:45:54 -0400

    var big

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,4 +1,5 @@
 function setup(){
+    let big = 400;
     createCanvas(600,800);
     background(0,50,205);
     noStroke();
@@ -7,5 +8,5 @@ function setup(){
     ellipse (60,80,100,100);
 
     fill(255);
-    arc(350, 500, 400, 400, PI, TWO_PI,);
+    arc(350, 500, big, big, PI, TWO_PI,);
 }
