e1d8942f90bd356623d41748471157a1eabf5f2f
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-02 12:04:48 -0400

    triangle

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,5 +1,6 @@
 function setup(){
     let big = 400;
+    let tiptop = 0;
     createCanvas(600,800);
     background(0,50,205);
     noStroke();
@@ -7,6 +8,12 @@ function setup(){
     fill(120, 0, 120);
     ellipse (60,80,100,100);
 
-    fill(255);
+
+    fill(210);
+    triangle(350, 400, 500, 400, 425, tiptop);
+
+
+    fill(210,105,30);
     arc(350, 500, big, big, PI, TWO_PI,);
+
 }
