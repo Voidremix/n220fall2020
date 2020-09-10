272ca9ff6f92a181eb515eca07239e2b13004276
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-10 13:21:54 -0400

    Update app.js

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -1,19 +1,17 @@
-function setup() {
+
+let xPos = 0;
+let yPos = 100;
+let xSpeed = 1.5;
+let ySpeed = -1.75;
+
+
+function draw() {
 
     createCanvas(600,800);
     background(0,50,205);
     noStroke();
 
-}
-
-function draw(){
-    let xPos = 0;
-    let yPos = 100;
-    let xSpeed = 1.5;
-    let ySpeed = -1.75;
-
     xPos = xPos + 2;
 	yPox = yPos + 2;
 	circle(xPos, yPos, 30)
-
 }
