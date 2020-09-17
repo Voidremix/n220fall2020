057557c3a6eb81cb9757729f6036411955da5f03
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-17 13:38:26 -0400

    Update app.js

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,12 +1,12 @@
 
-function setup() {
+function setup(){
     createCanvas(400,400);
 }
 
 function draw(){
     background(110);
 
-    for(var i = 0; i>4; i++) {
+    for(var i = 0; i<4; i++) {
 
         fill (255,0,0);
         circle(100,100, i *30);
