7cd625ad91964e78f62054de8834441384f069fd
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-17 13:30:46 -0400

    Update app.js

---
+++ labs/lab5/js/app.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw(){
+    background(255);
+
+    for(var i = 0; i>4; i++) {
+
+        fill (255,0,0);
+        rect(0, 0, 10, 10,);
+    }
+}
