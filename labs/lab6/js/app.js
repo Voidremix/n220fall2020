ed2f0d797791fbef14d3b6562ba50b1614733b24
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-18 16:15:13 -0400

    part1

--- labs/lab6/js/app.js
+++ labs/lab6/js/app.js
@@ -2,16 +2,3 @@ function setup() {
     createCanvas (500,500);
     background(110);
 }
-
-
-function draw(x) {
-
-    let x = '';
-  
-    for (let i = 1; i <= num; i++) {
-        rect(250, 250, 50, 50);
-        fill(255,0,0);
-      console.log(x += i);
-    }
-  }
-  draw(4);
