8ca80564adbc3d3a7e2bed407c61d5ab1fdcd484
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-18 16:45:49 -0400

    loops

--- labs/lab6/js/app.js
+++ labs/lab6/js/app.js
@@ -1,4 +1,16 @@
+
 function setup() {
     createCanvas (500,500);
     background(110);
+}
+
+function draw() {
+
+    for(x=1;x<5;x++){
+    
+        for(y=1;y< x+1;y++){
+            fill(255,0,0);
+            rect((y*15), (x*15), 15, 15); 
+        }
+    }
 }
---
+++ labs/lab7/js/app.js
@@ -0,0 +1,12 @@
+function setup () {
+    createCanvas (400,400);
+    background (100);
+}
+
+function draw(){
+    for(var i =50; i >0; i--) {
+
+        stroke (0);
+        circle(200,200, i*4);
+    }
+}
