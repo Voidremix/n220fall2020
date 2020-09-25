80acccbd762a7a875fdeedc4cafeac827683ec7c
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-09-25 13:32:53 -0400

    Update app.js

--- labs/lab8/js/app.js
+++ labs/lab8/js/app.js
@@ -1,11 +1,13 @@
-let MyArray = ["#f0faf0", "#331d18", "#ff638d"];
+let MyArray = ["#f0faf0", "#5e4a0c", "#ff638d"];
 
 function setup(){
     createCanvas(300,400);
+    background(0,200,0);
+    noStroke();
 
     for (var i=0; i < MyArray.length; i++) {
         fill( MyArray[i] );
-        rect(50 + (40*i)), 200, 50, 50);
+        rect(50 + (50*i), 100, 50, 50);
     }
 
 }
