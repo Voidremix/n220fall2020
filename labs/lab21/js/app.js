4d6e2d14a1044f5fedda24bbe589d7ebd587328d
Voidremix <60666385+Voidremix@users.noreply.github.com>
2020-11-06 19:38:23 -0500

    lab 21 & 22 setup

---
+++ labs/lab21/js/app.js
@@ -0,0 +1,16 @@
+
+document.getElementById("div").addEventListener("mouseover", mouseOver);
+
+document.getElementById("div").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+
+  document.getElementById("div").style.color = "#0c0c0d";
+
+}
+
+function mouseOut() {
+
+  document.getElementById("div").style.color = "#326fa8";
+
+}
---
+++ labs/lab22/js/app.js
@@ -0,0 +1,11 @@
+
+function divs(){
+
+    for(i = 0; i < newDiv.length; i++){
+
+        document.createElement("div");
+
+    }
+
+    document.appendChild(newDiv, null)
+}
